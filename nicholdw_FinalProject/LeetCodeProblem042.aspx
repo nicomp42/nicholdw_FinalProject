60fd6c1c7390e41834e978460d35a63ff82721eb
nicomp42 <nicholdw@ucmail.uc.edu>
2024-04-18 11:19:37 -0400

    Add project files.

---
+++ nicholdw_FinalProject/LeetCodeProblem042.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LeetCodeProblem042.aspx.cs" Inherits="nicholdw_FinalProject.LeetCodeProblem042" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="cmdLeetCodeProblem042" runat="server" Text="Probloem 042" OnClick="cmdLeetCodeProblem042_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ nicholdw_FinalProject/LeetCodeProblem042.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace nicholdw_FinalProject
+{
+    public partial class LeetCodeProblem042 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cmdLeetCodeProblem042_Click(object sender, EventArgs e)
+        {
+            // Open the Web Form for this LeetCode problem.
+        }
+    }
+}
